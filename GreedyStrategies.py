d14143cf75839f8e9b9e18171e84c16fa9e250a9
Condy <ck2860@rit.edu>
2020-10-13 20:01:52 -0700

    cleaning the code, no redundant code

--- GreedyStrategies.py
+++ GreedyStrategies.py
@@ -1,4 +1,5 @@
 import warnings
+
 warnings.filterwarnings('ignore', category=FutureWarning)
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 import matplotlib.pyplot as plt
@@ -8,9 +9,12 @@ import sys
 import tensorflow.compat.v1 as tf
 from ContextualBandit import *
 from ContextualBanditAgent import *
+
 # warnings.filterwarnings('ignore', category=FutureWarning)
 # warnings.filterwarnings('ignore', category=DeprecationWarning)
-# tf.logging.set_verbosity(tf.logging.ERROR)
+tf.logging.set_verbosity(tf.logging.ERROR)
+from InitializeTensor import *
+
 
 ## Documentation for Greedy Strategies class
 # this class has decreasing-epsilon, greedy-epsilon, hybrid#1, hybrid#2, hybrid#3, hybrid#4, and hybrid#5 functions.
@@ -27,34 +31,15 @@ class Greedystrategies():
     ##@param df1a: initialized dataframe
     def decreasingEpsilon(self, a1, df1a):
         for a in range(self.numberOfTrials):
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0]   # The weights we will evaluate to look into the network.
-            total_episodes = 10000  # Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
             e = 1  # start with highly explorative (100% explore, 0% exploit)
-            init = tf.global_variables_initializer()
-
             # Launch the tensorflow graph
             with tf.Session() as sess:
                 sess.run(init)
                 i = 0
                 np.random.seed(a1)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions)  # explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
-
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
@@ -64,7 +49,7 @@ class Greedystrategies():
                     e -= 0.0001  # in the end it would be highly exploitative
                     i += 1
                 a1 += 1
-                print("Trial#", a+1, ": Epsilon-Decreasing is done! ")
+                print("Trial#", a + 1, ": Epsilon-Decreasing is done! ")
         return df1a
 
     ## Documentation for Epsilon-Greedy function
@@ -73,33 +58,16 @@ class Greedystrategies():
     ##@param df2a: initialized dataframe
     def EpsilonGreedy(self, b, df2a):
         for a in range(self.numberOfTrials):
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
-            total_episodes = 10000  # Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
-            # this is the probability of the agent to explore. 10% to explore and 90% of exploit
-            e = 0.1
-            init = tf.global_variables_initializer()
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
+            e = 0.1  # this is the probability of the agent to explore. 10% to explore and 90% of exploit
+
             # Launch the tensorflow graph
             with tf.Session() as sess:
                 sess.run(init)
                 i = 0
                 np.random.seed(b)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions) # explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
-
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
@@ -108,7 +76,7 @@ class Greedystrategies():
                         # print("Epsilon-Greedy mean rewards: " + str(meanR[0]) + " at the episode of " + str(i))
                     i += 1
                 b += 1
-                print("Trial#", a+1, ": Epsilon-Greedy is done!")
+                print("Trial#", a + 1, ": Epsilon-Greedy is done!")
         return df2a
 
     ## Documentation for Hybrid #1 function
@@ -117,33 +85,15 @@ class Greedystrategies():
     ##@param df3a: initialized dataframe
     def hybrid1(self, c, df3a):
         for a in range(self.numberOfTrials):
-            # hybrid #1
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
-            total_episodes = 10000  # Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
             e = 0.9  # start with highly explorative (90% explore, 10% exploit)
-            init = tf.global_variables_initializer()
             # Launch the tensorflow graph
             with tf.Session() as sess:
                 sess.run(init)
                 i = 0
                 np.random.seed(c)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions)  # explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
-
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
@@ -153,7 +103,7 @@ class Greedystrategies():
                     e -= 0.00008  # in the end it would be highly exploitative (10% explore, 90% exploit)
                     i += 1
                 c += 1
-                print("Trial#", a+1, ": Hybrid#1 is done!")
+                print("Trial#", a + 1, ": Hybrid#1 is done!")
         return df3a
 
     ## Documentation for Hybrid #2 function
@@ -162,38 +112,18 @@ class Greedystrategies():
     ##@param df4a: initialized dataframe
     def hybrid2(self, d, df4a):
         for a in range(self.numberOfTrials):
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0] #The weights we will evaluate to look into the network.
-            total_episodes = 10000 #Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions]) #Set scoreboard for bandits to 0.
-            e = 1 #start with highly explorative (90% explore, 10% exploit)
-            init = tf.global_variables_initializer()
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
+            e = 1  # start with highly explorative (90% explore, 10% exploit)
             # Launch the tensorflow graph
             with tf.Session() as sess:
                 sess.run(init)
                 i = 0
                 np.random.seed(d)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions)  # explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
-
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
-
                         meanR = np.mean(total_reward, axis=1)
                         df4a = df4a.append({'x': i, 'y': meanR[0]}, ignore_index=True)
                         # print("Hybrid#2 mean rewards: " + str(meanR[0]) + " at the episode of "+ str(i))
@@ -202,7 +132,7 @@ class Greedystrategies():
                         e = 0.1
                     i += 1
                 d += 1
-                print("Trial#", a+1, ": Hybrid#2 is done!")
+                print("Trial#", a + 1, ": Hybrid#2 is done!")
         return df4a
 
     ## Documentation for Hybrid #3 function
@@ -211,42 +141,18 @@ class Greedystrategies():
     ##@param df5a: initialized dataframe
     def hybrid3(self, e1, df5a):
         for a in range(self.numberOfTrials):
-            # Hybrid#3
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
-            total_episodes = 10000  # Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
             e = 0.9  # start with highly explorative (90% explore, 10% exploit)
-
-            init = tf.global_variables_initializer()
-
             # Launch the tensorflow graph
             with tf.Session() as sess:
                 sess.run(init)
                 i = 0
                 np.random.seed(e1)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions)  # explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
-
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
-
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
-
                         meanR = np.mean(total_reward, axis=1)
                         df5a = df5a.append({'x': i, 'y': meanR[0]}, ignore_index=True)
                         # print("Hybrid#3 mean rewards: " + str(meanR[0]) + " at the episode of " + str(i))
@@ -255,7 +161,7 @@ class Greedystrategies():
                         e = 0.1
                     i += 1
                 e1 += 1
-                print("Trial#", a+1, ": Hybrid#3 is done!")
+                print("Trial#", a + 1, ": Hybrid#3 is done!")
         return df5a
 
     ## Documentation for Hybrid #4 function
@@ -264,39 +170,19 @@ class Greedystrategies():
     ##@param df6a: initialized dataframe
     def hybrid4(self, f, df6a):
         for a in range(self.numberOfTrials):
-            #Hybrid#4
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
-            total_episodes = 10000  # Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
             e = 1  # start with highly explorative (90% explore, 10% exploit)
 
-            init = tf.global_variables_initializer()
             # Launch the tensorflow graph
             with tf.Session() as sess:
                 sess.run(init)
                 i = 0
                 np.random.seed(f)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions)  # explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
-
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
-
                         meanR = np.mean(total_reward, axis=1)
                         df6a = df6a.append({'x': i, 'y': meanR[0]}, ignore_index=True)
                         # print("Hybrid#4 mean rewards: " + str(meanR[0]) + " at the episode of " + str(i))
@@ -305,7 +191,7 @@ class Greedystrategies():
                         e = 0.1
                     i += 1
                 f += 1
-                print("Trial#", a+1, ": Hybrid#4 is done!")
+                print("Trial#", a + 1, ": Hybrid#4 is done!")
         return df6a
 
     ## Documentation for Hybrid #5 function
@@ -314,15 +200,8 @@ class Greedystrategies():
     ##@param df7a: initialized dataframe
     def hybrid5(self, g, df7a):
         for a in range(self.numberOfTrials):
-            # Hybrid#5
-            tf.reset_default_graph()
-            cBandit = contextual_bandit()
-            myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
-            weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
-            total_episodes = 10000  # Set total number of episodes to train agent on.
-            total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+            cBandit, myAgent, weights, total_episodes, total_reward, init = initializeTensor()
             e = 0.9  # start with highly explorative (90% explore, 10% exploit)
-
             init = tf.global_variables_initializer()
 
             # Launch the tensorflow graph
@@ -331,25 +210,10 @@ class Greedystrategies():
                 i = 0
                 np.random.seed(g)
                 while i < total_episodes:
-                    s = cBandit.getBandit()  # Get a state from the environment.
-
-                    # Choose either a random action or one from our network.
-                    r = np.random.rand(1)
-                    if r < e:
-                        action = np.random.randint(cBandit.num_actions)  #explore
-                    else:
-                        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]}) #exploit
-
-                    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
-
-                    # Update the network.
-                    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
-                    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
-
+                    s, action, reward = BanditTensor(cBandit, e, sess, myAgent, weights)
                     # Update our running tally of scores.
                     total_reward[s, action] += reward
                     if i % 500 == 0:
-
                         meanR = np.mean(total_reward, axis=1)
                         df7a = df7a.append({'x': i, 'y': meanR[0]}, ignore_index=True)
                         # print("Hybrid#5 mean rewards: " + str(meanR[0]) + " at the episode of " + str(i))
@@ -358,5 +222,5 @@ class Greedystrategies():
                         e = 0.1
                     i += 1
                 g += 1
-                print("Trial#", a+1, ": Hybrid#5 is done!")
+                print("Trial#", a + 1, ": Hybrid#5 is done!")
         return df7a
---
+++ InitializeTensor.py
@@ -0,0 +1,44 @@
+import warnings
+
+warnings.filterwarnings('ignore', category=FutureWarning)
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sys
+import tensorflow.compat.v1 as tf
+from ContextualBandit import *
+from ContextualBanditAgent import *
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+## Documentation for Agent class
+# This class sets up a contextual bandit agent via building a feed-forward in the network and training/updating the network. It would either return positive or negative reward.  Positive reinforcement is a reward for picking the most optimal arm; negative reinforcement is a penalty for not picking the best arm.
+
+def initializeTensor():
+    tf.reset_default_graph()
+    cBandit = contextual_bandit()
+    myAgent = agent(lr=0.005, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)
+    weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
+    total_episodes = 10000  # Set total number of episodes to train agent on.
+    total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+    init = tf.global_variables_initializer()
+    return cBandit, myAgent, weights, total_episodes, total_reward, init
+
+
+def BanditTensor(cBandit, e, sess, myAgent, weights):
+    s = cBandit.getBandit()  # Get a state from the environment.
+    # Choose either a random action or one from our network.
+    r = np.random.rand(1)
+    if r < e:
+        action = np.random.randint(cBandit.num_actions)  # explore
+    else:
+        action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})  # exploit
+    reward = cBandit.pullArm(action)  # Get our reward for taking an action given a bandit.
+
+    # Update the network.
+    feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action],
+                 myAgent.state_in: [s]}
+    _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
+    return s, action, reward
